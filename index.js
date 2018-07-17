2982c4d6a9723260e5edfa6e2aeb85955a4e2dc7
ahmad Abdulkhaliq Osman <ahmadabdulkhaliq384@gmail.com>
2018-07-17 22:11:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ const excitedWelcomeMessage='WELCOME TO FLATBOOK, '+upperCaseCurrentUser+ '!';
 
 const shortGreeting='Welcome, '
 
+ const firstInitial = currentUser[0];
+      const restOfName = currentUser.slice(1);
+
+     const shortGreeting=firstInitial);
+      
